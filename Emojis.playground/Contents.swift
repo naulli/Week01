2376834c9182fdb0b1fe8e417a43acea388a8423
Joanna Margareth Nauli <18220007@mahasiswa.itb.ac.id>
2023-01-31 16:39:31 -0500

    Update homework

--- Emojis.playground/Contents.swift
+++ Emojis.playground/Contents.swift
@@ -7,3 +7,14 @@ let origin = "🇮🇩"
 let sport = "🧘🏻‍♀️"
 let text = "I'm from \(origin) and the sport activity I love the most is \(sport) "
 print(text)
+
+
+// Trial 2
+let str = "❤️🧡💛💚💙💜"
+//  for uni in str {
+//    print(uni)
+//  }
+print("str", str)
+print(str.count)
+print(str[str.startIndex])
+print(str[str.index(str.startIndex, offsetBy: 3)])
