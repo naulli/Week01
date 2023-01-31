6dcb5ee6594a5ee57e365f42467938389b9c9b92
Joanna Margareth Nauli <18220007@mahasiswa.itb.ac.id>
2023-01-31 15:21:46 -0500

    Homework commit

---
+++ Emojis.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var greeting = "Homework #1"
+
+// Trial
+var emojis = "Most Recent Used Emojis"
+
+for _ in 1...5 {
+    emojis += " 🙏 "
+}
+
+print(emojis)
+
+// Trial #2
+let origin = "🇮🇩"
+let sport = "🧘🏻‍♀️"
+let message = "I'm from \(origin) and the sport activity I love the most is \(sport) "
+print(message)
